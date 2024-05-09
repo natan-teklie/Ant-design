fca37bb290bb0677c28bca259133055cee3d7d4c
natan-teklie <natanmitiku232@gmail.com>
2024-05-09 18:11:52 +0300

    fixed

---
+++ antd-demo/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import './App.css';
+import { Button } from "antd";
+import "./App.css"
+
+
+const App = () => {
+  return (
+    <div className='aple'><Button>Ant Design-ReactJs</Button></div>
+  )
+}
+
+export default App
